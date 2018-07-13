e023280bece2b2d4ff4643ad96b948dc90bdf03c
m.horiachev <m.horiachev@levi9.com>
2018-07-13 17:09:37 +0300

    softdelteble start

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -9,12 +9,51 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PostController extends Controller {
-//getFilters()->enable('soft-deleteable');
 
-//$filter = $em->getFilters()->enable('soft-deleteable');
-//$filter->disableForEntity('Entity\Article');
-//$filter->enableForEntity('Entity\Article');
+  /**
+   * @Route("/{category_code}/posts", name="category_show_posts")
+   * @Method("GET")
+   */
+  public function getPostAction() {
+
+    $em = $this->getDoctrine()->getManager();
+
+    $posts = $em->getRepository('AppBundle:Post')
+      ->findAll();
+
+//    dump($posts);die();
+    return $this->render('blog/posts.html.twig', [
+      'posts' => $posts,
+    ]);
+  }
+
+
+
+  //  /**
+  //   * @Route("/genus/{name}/notes", name="genus_show_notes")
+  //   * @Method("GET")
+  //   */
+  //
+  //  public function getNotesAction(Genus $genus) {
+  //    foreach ($genus->getNotes() as $note) {
+  //      $notes[] = [
+  //        'id' => $note->getId(),
+  //        'username' => $note->getUsername(),
+  //        'avatarUri' => '/images/' . $note->getUserAvatarFilename(),
+  //        'note' => $note->getNote(),
+  //        'date' => $note->getCreatedAt()->format('M d, Y'),
+  //      ];
+  //    }
+  //
+  //    $data = [
+  //      'notes' => $notes,
+  //    ];
+  //
+  //    return new JsonResponse($data);
+  //  }
 }
